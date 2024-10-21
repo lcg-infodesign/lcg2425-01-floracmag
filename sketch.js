7a88ae9d5bd226c531f16b1998275e8d1d334054
floracmag <flora.carneiro@mail.polimi.it>
2024-10-21 08:15:45 -0300

    first 21/10

--- sketch.js
+++ sketch.js
@@ -1,17 +1,68 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  angle = random(TWO_PI); // Gera um ângulo randômico
+  noLoop();
 }
 
 function draw() {
-  // put drawing code here
+  background("#132b95");
+
+  noStroke();
+  fill("#c24435");
+
+let columns = windowWidth / (100 + 10);
+let rows = windowHeight / (100 + 10);
+
+// 1 grande
+for (let i = 0.1; i < columns; i++) {
+  for (let r = 0.1; r < rows; r++){
+    rect(i * 220, r * 220, 100, 100);
+  }
+}
+
+// 2 grande
+for (let i = 0.61; i < columns; i++) {
+  for (let r = 0.61; r < rows; r++){
+    rect(i * 220, r * 220, 100, 100);
+  }
+}
+
+//1 pequeno
+for (let i = 0.63; i < columns; i++) {
+  for (let r = 0.125; r < rows; r++) {
+      // Gera um ângulo randômico entre 0 e 5 graus
+      let angle = random(-5,5) * (PI / 180); // Converte para radianos
+      // Calcula a posição central do retângulo
+      let x = i * 220 + 45; // 90/2 = 45
+      let y = r * 220 + 45; // 90/2 = 45
+
+      // Translada para o centro do retângulo
+      push(); // Salva o estado atual do contexto
+      translate(x, y);
+      rotate(angle); // Rotaciona pelo ângulo randômico
+      rectMode(CENTER); // Define o modo de desenho para o centro
+      rect(0, 0, 85, 85); // Desenha o retângulo
+      pop(); // Restaura o estado anterior do contexto
+  }
+}
+
+//2 pequeno
+for (let i = 0.125; i < columns; i++) {
+  for (let r = 0.63; r < rows; r++) {
+      // Gera um ângulo randômico entre 0 e 5 graus
+      let angle = random(-5,5) * (PI / 180); // Converte para radianos
+      // Calcula a posição central do retângulo
+      let x = i * 220 + 45; // 90/2 = 45
+      let y = r * 220 + 45; // 90/2 = 45
+
+      // Translada para o centro do retângulo
+      push(); // Salva o estado atual do contexto
+      translate(x, y);
+      rotate(angle); // Rotaciona pelo ângulo randômico
+      rectMode(CENTER); // Define o modo de desenho para o centro
+      rect(0, 0, 85, 85); // Desenha o retângulo
+      pop(); // Restaura o estado anterior do contexto
+  }
+}
+
 }
